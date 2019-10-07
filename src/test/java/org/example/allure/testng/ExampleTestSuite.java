901fda5701d2462dd8e53a0ed2dba94071c383d7
Mehmet Şirin Usanmaz <musanmaz@snapbytes.com>
2019-10-07 11:14:07 +0300

    first commit

---
+++ src/test/java/org/example/allure/testng/ExampleTestSuite.java
@@ -0,0 +1,20 @@
+package org.example.allure.testng;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+public class ExampleTestSuite {
+
+    @Test(description = "Successful test")
+    public void successfulTest() {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com.tr");
+
+    }
+
+}
